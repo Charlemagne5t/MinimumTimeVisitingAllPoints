367503bea2ec9ef9140a7c1532ea7b1c9373ac64
alek <wait82985@gmail.com>
2026-01-12 14:45:47 +0500

    problem solved

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,11 +1,14 @@
 class Solution {
     public int minTimeToVisitAllPoints(int[][] points) {
-        int sum = 0;
-        for(int i = 1; i < points.length; i++){
-            sum += Math.max(
-                    Math.abs(points[i][0] - points[i - 1][0]), Math.abs(points[i][1] - points[i - 1][1]));
+        int res = 0;
+        for (int i = 1; i < points.length; i++) {
+            int x1 = points[i - 1][0];
+            int y1 = points[i - 1][1];
+            int x2 = points[i][0];
+            int y2 = points[i][1];
+            res += Math.min(Math.abs(x1 - x2), Math.abs(y1 - y2)) + Math.max(Math.abs(x1 - x2), Math.abs(y1 - y2)) - Math.min(Math.abs(x1 - x2), Math.abs(y1 - y2));
         }
 
-        return sum;
+        return res;
     }
 }
